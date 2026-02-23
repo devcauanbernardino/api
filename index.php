4e357c5e43e1fdb0772ee30b0d7ddb1b23dd7225
devcauanbernardino <devcauanbernardino@gmail.com>
2026-02-23 18:15:01 -0300

    aula sobre middleware

--- index.php
+++ index.php
@@ -3,42 +3,170 @@
 use MyApp\View;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 require 'vendor/autoload.php';
 
 $app = new \Slim\App([
-        'settings' => [
-            'displayErrorDetails' => true
-        ]
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
 ]);
 
-//Container depency injection
-class Servico {}
-$servico = new Servico();
-
-//Container Pimple
 $container = $app->getContainer();
-$container['Home'] = function () {
-    return new MyApp\controllers\Home(new MyApp\View);
+$container['db'] = function () {
+
+    $capsule = new Capsule;
+    $capsule->addConnection([
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'slim',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ]);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+    
+    return $capsule;
 };
 
-$app->get('/servico', function (Request $request, Response $response) {
-    $servico = $this->get('servico');
-    var_dump($servico);
+$app->get('/usuarios', function (Request $request, Response $response) {
+    $db = $this->get('db');
+    //Criar Tabela
+    // $db->schema()->dropIfExists('usuarios');
+    // $db->schema()->create('usuarios', function($table) {
+    //     $table->increments('id');
+    //     $table->string('nome');
+    //     $table->string('email');
+    //     $table->timestamps();
+    // });
+
+    //Inserir dados para a tabela usuarios
+    // $db->table('usuarios')->insert([
+    //     'nome' => 'Jamilton Damasceno',
+    //     'email' => 'jamilton@gmail.com'
+    // ]);
+
+    //Atualizar
+    // $db->table('usuarios')->where('id', 1)->update([
+    //     'nome' => 'Jorge',
+    // ]);
+
+    //Deletar
+    // $db->table('usuarios')->where('id', 2)->delete();
+
+    //Listar
+    $usuarios = $db->table('usuarios')->get();
+    foreach($usuarios as $usuario) {
+        echo $usuario->nome . '<br>';
+
+    }
 });
 
-//Controllers como servico
-$app->get('/usuario', 'Home:index');
+
 
 $app->run();
 
+//-----------------------------------------------------------------------------------------
+
+//Tipos de respostas: cabecalho, texto, json, xml
+
+// $app->get('/header', function (Request $request, Response $response) {
+//     $response->write('Esse e um retorno header');
+//     return $response->withHeader('allow', 'PUT')->withAddedHeader('Content-Length', 10);
+// });
+
+// $app->get('/json', function (Request $request, Response $response) {
+//     return $response->withJson([
+//         "nome" => "Cauan",
+//         "endereco" => "Endereco tal.."
+//     ]);
+// });
+
+// $app->get('/xml', function (Request $request, Response $response) {
+//     $xml = file_get_contents('arquivo');
+//     $response->write($xml);
+
+//     return $response->withHeader('Content-Type', 'application/xml');
+// });
+
+
+//-----------------------------------------------------------------------------------------
+
+//Middleware
+
+// $app->add( function($request, $response, $next) {
+//     $response->write('Inicio camada 1 + ');
+//     // return $next($request, $response);
+//     $response = $next($request, $response);
+//     $response->write(' + Fim da camada 1');
+//     return $response;
+// });
+
+// $app->add( function($request, $response, $next) {
+//     $response->write('Inicio camada 2 + ');
+//     // return $next($request, $response);
+//     $response = $next($request, $response);
+//     $response->write(' + Fim da camada 2');
+//     return $response;
+// });
+
+
+// $app->add( function($request, $response, $next) {
+//     $response->write('Inicio camada 2 + ');
+//     return $next($request, $response);
+// });
+
+// $app->get('/usuarios', function (Request $request, Response $response) {
+//     $response->write('Acao principal usuarios');
+
+// });
+
+// $app->get('/postagens', function (Request $request, Response $response) {
+//     $response->write('Acao principal postagens');
+
+// });
+
+
+//-----------------------------------------------------------------------------------------
+
+
+//Container depency injection
+
+// class Servico {}
+// $servico = new Servico();
+
+// //Container Pimple
+// $container = $app->getContainer();
+// $container['Home'] = function () {
+//     return new MyApp\controllers\Home(new MyApp\View);
+// };
+
+// $app->get('/servico', function (Request $request, Response $response) {
+//     $servico = $this->get('servico');
+//     var_dump($servico);
+// });
+
+// //Controllers como servico
+// $app->get('/usuario', 'Home:index');
+
+
+//-----------------------------------------------------------------------------------------
+
 //Padrao PSR7
+
 // $app->get('/postagens', function (Request $request, Response $response) {
 //     //Escreve no corpo da resposta utilizando o padrao PSR7
 //     $response->getBody()->write("Lista postagens");
 //     return $response;
 //     //echo "";
 // });
+
+//-----------------------------------------------------------------------------------------
 
 /* 
 Tipos de requisicao ou Verbos HTTP
